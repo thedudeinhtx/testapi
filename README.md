5910e081f24899be167d56292c2e20a0a60b3c0f
Robert West <robert@obscureapi.com>
2024-10-24 13:13:13 -0500

    formatted all

---
+++ README.md
@@ -0,0 +1,2 @@
+# testapi
+This is just a stupid simple "api" jumping off point written in go. Its just used as a starting point to test things out or a test repo for build pipelines.
--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+func (app *Config) Default(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the api",
